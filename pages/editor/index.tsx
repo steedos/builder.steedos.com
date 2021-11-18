7306b491579121fea6665e638a7bebd9b0f844c0
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2021-11-18 09:57:08 +0800

    serverless

--- pages/editor/index.tsx
+++ pages/editor/index.tsx
@@ -11,7 +11,67 @@ loader.config({ paths: { vs: "/npm/monaco-editor/0.30.1/min/vs" } });
 
 
 export default function Editor() {
+  const params = {
+    "acceptSuggestionOnCommitCharacter": true,
+    "acceptSuggestionOnEnter": "on",
+    "accessibilitySupport": "auto",
+    "autoIndent": false,
+    "automaticLayout": true,
+    "codeLens": true,
+    "colorDecorators": true,
+    "contextmenu": true,
+    "cursorBlinking": "blink",
+    "cursorSmoothCaretAnimation": false,
+    "cursorStyle": "line",
+    "disableLayerHinting": false,
+    "disableMonospaceOptimizations": false,
+    "dragAndDrop": false,
+    "fixedOverflowWidgets": false,
+    "folding": true,
+    "foldingStrategy": "auto",
+    "fontLigatures": false,
+    "formatOnPaste": false,
+    "formatOnType": false,
+    "hideCursorInOverviewRuler": false,
+    "highlightActiveIndentGuide": true,
+    "links": true,
+    "mouseWheelZoom": false,
+    "multiCursorMergeOverlapping": true,
+    "multiCursorModifier": "alt",
+    "overviewRulerBorder": true,
+    "overviewRulerLanes": 2,
+    "quickSuggestions": true,
+    "quickSuggestionsDelay": 100,
+    "readOnly": false,
+    "renderControlCharacters": false,
+    "renderFinalNewline": true,
+    "renderIndentGuides": true,
+    "renderLineHighlight": "all",
+    "renderWhitespace": "none",
+    "revealHorizontalRightPadding": 30,
+    "roundedSelection": true,
+    "rulers": [],
+    "scrollBeyondLastColumn": 5,
+    "scrollBeyondLastLine": true,
+    "selectOnLineNumbers": true,
+    "selectionClipboard": true,
+    "selectionHighlight": true,
+    "showFoldingControls": "mouseover",
+    "smoothScrolling": false,
+    "suggestOnTriggerCharacters": true,
+    "wordBasedSuggestions": true,
+    "wordSeparators": "~!@#$%^&*()-=+[{]}|;:'\",.<>/?",
+    "wordWrap": "off",
+    "wordWrapBreakAfterCharacters": "\t})]?|&,;",
+    "wordWrapBreakBeforeCharacters": "{([+",
+    "wordWrapBreakObtrusiveCharacters": ".",
+    "wordWrapColumn": 80,
+    "wordWrapMinified": true,
+    "wrappingIndent": "none"
+  }
   return (
-      <MonacoEditor height="80vh"/>
+      <MonacoEditor height="80vh"
+        {...params}
+      />
   )
 }
